c7caa8f5237ee8f50e0636d32aebc37de5b991f0
Heri Setyawan <herisetyawan233@gmail.com>
2025-08-12 22:06:35 +0700

    test

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -79,10 +79,6 @@ class AuthController extends Controller
         ) {
             return response()->json([
                 'message' => 'OTP tidak valid atau sudah kedaluwarsa',
-                "error" => [now()->gt($user->otp_expires_at), $user->otp !== $request->otp, !$user->otp],
-                "otp" => $user->otp,
-                "otp_expires_at" => $user->otp_expires_at,
-                "request_otp" => $request->otp
             ], 401);
         }
 
